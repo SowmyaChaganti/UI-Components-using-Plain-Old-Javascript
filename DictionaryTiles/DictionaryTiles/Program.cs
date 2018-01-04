6299923f590b55db3153e1c4ba082570c7920c3c
Sowmya Chaganti <chaganti.sowmya@gmail.com>
2018-01-04 14:16:43 -0500

    Add files via upload

---
+++ DictionaryTiles/DictionaryTiles/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DictionaryTiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> allWords = new List<string>();
+            ReadFile read = new ReadFile();
+
+            allWords = read.readFile();
+            Console.WriteLine("Welcome to Dictionary!!");
+            Console.WriteLine();
+            Console.WriteLine("Enter seven tiles (tile: '_' Eg: a _ _ _ t _ m) : ");
+            String tiles = Console.ReadLine();
+            tiles = tiles.Replace(" ", "");
+            tiles = tiles.Replace("_", "[a-z]?");
+
+            Regex pattern = new Regex(@"^"+tiles+"$");
+
+
+            foreach (String s in allWords)
+            {
+                if (pattern.IsMatch(s))
+                {
+                    Console.WriteLine(s);
+                }
+            }   
+            
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ DictionaryTiles/DictionaryTiles/ReadFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryTiles
+{
+    class ReadFile
+    {
+        List<String> allWords = new List<String>();
+        public List<String> readFile()
+        {
+
+            String path = AppDomain.CurrentDomain.BaseDirectory;
+
+            path = path.Replace("bin\\Debug\\", "dictionary.txt");
+            System.IO.StreamReader file = new System.IO.StreamReader(path);
+            String line;
+            while((line = file.ReadLine()) != null)
+            {
+                //used list collection as this is best for accessing and reading
+                allWords.Add(line);
+
+            }
+
+            return allWords;
+        }
+    }
+}
